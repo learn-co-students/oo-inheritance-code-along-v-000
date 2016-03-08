a2627fbb2f9f80dadba5d9916eb6dec419dc4e49
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-03-08 09:20:32 -0800

    Done.

--- lib/super_vehicle.rb
+++ lib/super_vehicle.rb
@@ -1,5 +1,13 @@
 class Vehicle
-
- 
-    
+  attr_accessor :wheel_number, :wheel_size
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+  def fill_up_tank
+    return "filling up!"
+  end
+  def go 
+    return "vrrrrrrrooom!"
+  end
 end
