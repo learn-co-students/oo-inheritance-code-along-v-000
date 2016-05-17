4698ccd0bb5212aa9c2fc29b1d77def7e502f0a4
kaileeagray <kaileeagray@gmail.com>
2016-05-17 16:05:39 -0500

    Done.

--- lib/sub_car.rb
+++ lib/sub_car.rb
@@ -1,7 +1,8 @@
 require_relative "./super_vehicle.rb"
 
 class Car < Vehicle
-
+  #Since the compiler finds a go method within Car, this #go is executed.
+  #But, #fill_up_tank does not exist in Car so the compiler looks to its parent, Vehicle.
   def go
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
