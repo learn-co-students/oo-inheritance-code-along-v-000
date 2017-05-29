0d3db0109380b34bfe483d16beded3ec8156c855
Derrick Chu <dc79627n@pace.edu>
2017-05-29 12:18:50 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,15 @@
 require_relative "./vehicle.rb"
+
+#defining subclass
+
+# use < to inherit Car class from Vehicle
+
+class Car < Vehicle
+
+  #rewrite inherted go method, specific to one car
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+end
