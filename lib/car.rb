a8675db28c10998ba0eb38bd034d6b5df40b415f
Naomi Dennis <njdennis94@gmail.com>
2017-08-06 21:11:41 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,27 @@
 require_relative "./vehicle.rb"
+class Car
+=begin
+  Variables
+    wheel_size
+    wheel_number
+  Functions
+    init(string, number)
+    go : string
+    fill_up_tank : string
+=end
+  attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheel_size, wheel_num)
+    @wheel_Size = wheel_size
+    @wheel_number = wheel_num
+  end
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+  def fill_up_tank
+    'filling up!'
+  end
+
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,26 @@
 class Vehicle
+=begin
+  Variables
+    wheel_size
+    wheel_number
+  Functions
+    init(string, number)
+    go : string
+    fill_up_tank : string
+=end
+  attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheel_size, wheel_num)
+    @wheel_Size = wheel_size
+    @wheel_number = wheel_num
+  end
+
+  def go
+    'vrrrrrrrooom!'
+  end
+
+  def fill_up_tank
+    'filling up!'
+  end
 
 end
