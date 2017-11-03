7bc3fabb8e190847243f2e1fa36e8ab4951a07b4
Alex Crisan <alexcrisan2005@hotmail.com>
2017-11-03 14:28:50 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,10 @@
-require_relative "./vehicle.rb"
+require_relative "./vehicle.rb" #gives Car class access to the Vehicle class through it's file
+
+class Car < Vehicle #use the < to inherit Car class from Vehicle
+  #our Car class is inheriting all the methods defined in the Vehicle class.
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!" #this overwrites the #go method in the Vehicle class.
+  end
+
+end
