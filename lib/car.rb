4a5a9a1e1fafb8121af5a210814f4745636d3f24
Pamela Frieze <pfrieze@gmail.com>
2017-11-23 15:05:20 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,7 @@
-require_relative "./vehicle.rb"
+require_relative "./vehicle.rb"  #this class needs access to the vehicle class
+#avoid repititious code
+class Car < Vehicle  #inherit the car class from vehicle
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
