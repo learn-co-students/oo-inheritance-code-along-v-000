00cf550002d73a7afe565c4cdc34bed18378f32b
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2017-12-31 00:52:07 +0000

    ☠ RAW CODE

--- lib/car.rb
+++ lib/car.rb
@@ -1,11 +1,7 @@
 require_relative "./vehicle.rb"
-#==========================================
-  # SUB-CLASS
-#==========================================
+
 class Car < Vehicle
-#==========================================  
-  # INSTANCE METHODS
-#==========================================   
+  
   def go
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,21 +2,16 @@
   # SUPER CLASS
 #========================================== 
 class Vehicle
-#==========================================
-  # INSTANCE ATTRIBUTES
-#==========================================
+
   attr_accessor :wheel_size, :wheel_number
-#==========================================  
-  # INSTANCE METHODS
-#========================================== 
+
   def initialize(w_size, w_num)
     self.wheel_size = w_size
     self.wheel_number = w_num
   end
  
   def go
-    "vrrrrrrrooom!"
-  end
+    "vrrrrrrrooom!"  end
  
   def fill_up_tank
     "filling up!"
