ca29487cb690b47e0e2ef86b5a4126ca65d2decc
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2017-12-31 00:51:31 +0000

    ☕ SOLUTION

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,13 @@
 require_relative "./vehicle.rb"
+#==========================================
+  # SUB-CLASS
+#==========================================
+class Car < Vehicle
+#==========================================  
+  # INSTANCE METHODS
+#==========================================   
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+  
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,25 @@
+#==========================================
+  # SUPER CLASS
+#========================================== 
 class Vehicle
-
+#==========================================
+  # INSTANCE ATTRIBUTES
+#==========================================
+  attr_accessor :wheel_size, :wheel_number
+#==========================================  
+  # INSTANCE METHODS
+#========================================== 
+  def initialize(w_size, w_num)
+    self.wheel_size = w_size
+    self.wheel_number = w_num
+  end
+ 
+  def go
+    "vrrrrrrrooom!"
+  end
+ 
+  def fill_up_tank
+    "filling up!"
+  end
+ 
 end
