d9fcda73b03babb6fc0a57ae6261bcd30314e99c
avelineamour <razelle@satyaorthopedic.com>
2018-01-24 19:05:49 -0500

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,18 @@
-class Vehicle
 
+
+class Vehicle
+  attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheelsize, wheelnumber)
+    @wheel_size = wheelsize
+    @wheel_number = wheelnumber
+  end
+
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
 end
