30c15104ee3c4f9b054f428d7c13388c7f113e92
EPIC448 <samrey2018@gmail.com>
2018-01-27 05:32:54 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,7 @@
 require_relative "./vehicle.rb"
+class Car < Vehicle   #We use the < to inherit the Car class from Vehicle. Notice that there are no methods defined in the Car class.
+      #Overwriting the go method.. acording to the test.
+      def go
+          "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+      end
+end
