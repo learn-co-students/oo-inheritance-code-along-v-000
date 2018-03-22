6046e3c5348bf8407b3299246a436d99946c35e4
Christine Cua <cloudycua@gmail.com>
2018-03-22 13:30:28 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,13 @@
+# Child/Subclass
+
 require_relative "./vehicle.rb"
+
+class Car < Vehicle  # inherits the Car class from Vehicle
+
+# compiler looks at class that method being called on first, then parent.
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+end
