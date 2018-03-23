0f3226fca9c4cfffaf5b02edfeade5b97cbbb62e
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-23 20:57:24 +0000

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,20 @@
 class Vehicle
+  attr_accessor :wheel_size, :wheel_number
 
-end
+  # initializes with the wheel_size and wheel_number as parameters
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end # initialize
+
+  # outputs a vroom sound
+  def go
+    "vrrrrrrrooom!"
+  end # go
+
+  # outputs a message for filling up
+  def fill_up_tank
+    "filling up!"
+  end # filling_up_tank
+
+end # class Vehicle
