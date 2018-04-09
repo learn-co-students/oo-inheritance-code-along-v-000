df2a5e44660d0b0dea40254c2c90623be5f7a878
Hiroki Kato <hkato@sas.upenn.edu>
2018-04-09 20:21:24 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,8 @@
 require_relative "./vehicle.rb"
+#require relative so that our Car class can access Vehicle class
+
+class Car < Vehicle
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
