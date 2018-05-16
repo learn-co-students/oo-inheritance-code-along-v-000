b68ef3a53240f1cdf3d57223ef6064629928d23b
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-16 20:21:25 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,8 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle #the car requires vehicle because it's inheriting from vehicle
+  #and the vehicle overwrites the go method.
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
