3fa5fae3347b1706babf92d8cfb51b85b2b5a22c
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-23 16:37:21 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,8 @@
-require_relative "./vehicle.rb"
+require_relative "./vehicle.rb" #the Car class will need access to the Vehicle class
+
+class Car < Vehicle # we use < to inherit the Car class from Vehicle. There are no methods defined in Car class
+  def go #Overwriting the go method in the Vehicle class
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,16 @@
-class Vehicle
+class Vehicle #parent or super class
+  attr_accessor :wheel_size, :wheel_number
 
+  def initialize(wheel_size, wheel_number) #instances of Vehicle initialize with a wheel size and number
+    @wheel_size = wheel_size
+    @wheel_number = wheel_size
+  end
+
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
 end
