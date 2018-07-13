db2a6d8e28f5f5219cac1537dd232f9bb1296504
Michael Morris <mdam2345@gmail.com>
2018-07-13 02:19:07 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,23 +1,4 @@
 require_relative "./vehicle.rb"
-
-class Car
-  attr_accessor :wheel,  :number
-
-  def initialize(wheel, number)
-    @wheel = wheel
-    @number = number
-
-    def wheel_size
-
-    end
-
-    def wheel_number
-    end
-
-    def go
-    end
-
-    def fill_up_tank
-    end
-  end
+class Car < Vehicle
+ 
 end
