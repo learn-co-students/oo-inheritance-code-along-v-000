2b8d0b8174a0628f64c53033ac881faa73cc74ab
Justine Bricknell <justinegayton@gmail.com>
2018-07-28 22:34:08 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,7 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle
+  def go #overwrite the method inherited from vehicle, specifc to this car method only 
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
