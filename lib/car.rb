c0e18bc74c9cd8f85f00fa9bec66cf3b664d16ed
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-08-23 01:51:29 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,9 @@
+require 'pry'
 require_relative "./vehicle.rb"
+
+class Car < Vehicle #requires arguements of Vehicle Class (wheel_size, wheel_number)
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+  # binding.pry
+end
